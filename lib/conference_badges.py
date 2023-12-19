7168b413f02b1077a23148d9ca23a94937b921e2
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-12-19 13:57:32 -0600

    finished lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    greeting = "Hello, my name is " + name + "."
+    return greeting
 
 def batch_badge_creator(names):
-    return None
+    list = []
+    for name in names:
+        greeting = badge_maker(name)
+        list.append(greeting)
+    return list
 
 def assign_rooms(names):
-    return None
+    list = []
+    for name in names:
+        room = names.index(name) + 1
+        greeting = "Hello, " + name + "! You'll be assigned to room " + str(room) + "!"
+        list.append(greeting)
+    return list
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
